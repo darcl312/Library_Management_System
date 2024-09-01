43f8931bfbd5a3c85f9181e32f7ad9940ad7c19f
HDarshil <darcl162004@gmail.com>
2024-09-01 19:16:23 +0530

    create the add book function

--- src/main/java/com/mycompany/library_management_system/book.java
+++ src/main/java/com/mycompany/library_management_system/book.java
@@ -17,10 +17,41 @@ package com.mycompany.library_management_system;
             this.publicationYear = publicationYear;
             this.isBorrowed = false;
         }
-
+    
+        public String getIsbn() {
+            return isbn;
+        }
         
     
-      
+        public String getTitle() {
+            return title;
+        }
+    
+        public String getAuthor() {
+            return author;
+        }
+    
+        public int getPublicationYear() {
+            return publicationYear;
+        }
+    
+        public boolean isBorrowed() {
+            return isBorrowed;
+        }
+    
+        public void borrow() {
+            
+            isBorrowed = true;
+        }
+    
+        public void returnBook() {
+            isBorrowed = false;
+        }
+    
+        @Override
+        public String toString() {
+            return title + " by " + author + " (ISBN: " + isbn + ", Year: " + publicationYear + ")";
+        }
     }
     
     
